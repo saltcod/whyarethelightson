2012bcd4036ad93fdd7dbc55a91cd3595f217761
Terry Sutton <saltcod@gmail.com>
2013-08-14 15:58:57 -0230

    set up get template part

--- content.php
+++ content.php
@@ -1,21 +1,20 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
 
-	<header class="article-header">
+ 
 
-		<h1 class="h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-
-	</header> <!-- end article header -->
-
+ 
 	<section class="entry-content clearfix">
 		<?php if ( has_post_thumbnail() ) {
 			the_post_thumbnail('full');
+			whylights_the_post_thumbnail_caption();
 		} ?>
+		<h1 class="post-title h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
 		<?php the_content(); ?>
 	</section> <!-- end article section -->
 
 	<footer class="article-footer">
-		<p class="tags"><?php the_tags('<span class="tags-title">' . __('Tags:', 'bonestheme') . '</span> ', ', ', ''); ?></p>
+		<p class="tags"><?php the_tags('<span class="tags-title">' . __('<i class=icon-tag></i>', 'bonestheme') . '</span> ', ', ', ''); ?></p>
 
 	</footer> <!-- end article footer -->
 
--- single.php
+++ single.php
@@ -1,61 +1,27 @@
 <?php get_header(); ?>
+<?php get_sidebar(); ?>
+<div id="content">
 
-			<div id="content">
+	<div id="inner-content" class="wrap clearfix">
 
-				<div id="inner-content" class="wrap clearfix">
+		<div id="main" class="eightcol last clearfix" role="main">
 
-					<div id="main" class="eightcol first clearfix" role="main">
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			<?php get_template_part( 'content', get_post_format() );?>
+		<?php endwhile; ?>
 
-						<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	<?php else : ?>
+	<?php get_template_part( 'no-results', 'index' ); ?>
+<?php endif; ?>
 
-							<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+<?php comments_template(); ?>
 
-								<header class="article-header">
+</div> <!-- end #main -->
 
-									<h1 class="entry-title single-title" itemprop="headline"><?php the_title(); ?></h1>
-									<p class="byline vcard"><?php
-										printf(__('Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span> <span class="amp">&amp;</span> filed under %4$s.', 'bonestheme'), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), bones_get_the_author_posts_link(), get_the_category_list(', '));
-									?></p>
 
-								</header> <!-- end article header -->
 
-								<section class="entry-content clearfix" itemprop="articleBody">
-									<?php the_content(); ?>
-								</section> <!-- end article section -->
+</div> <!-- end #inner-content -->
 
-								<footer class="article-footer">
-									<?php the_tags('<p class="tags"><span class="tags-title">' . __('Tags:', 'bonestheme') . '</span> ', ', ', '</p>'); ?>
-
-								</footer> <!-- end article footer -->
-
-								<?php comments_template(); ?>
-
-							</article> <!-- end article -->
-
-						<?php endwhile; ?>
-
-						<?php else : ?>
-
-							<article id="post-not-found" class="hentry clearfix">
-									<header class="article-header">
-										<h1><?php _e("Oops, Post Not Found!", "bonestheme"); ?></h1>
-									</header>
-									<section class="entry-content">
-										<p><?php _e("Uh Oh. Something is missing. Try double checking things.", "bonestheme"); ?></p>
-									</section>
-									<footer class="article-footer">
-											<p><?php _e("This is the error message in the single.php template.", "bonestheme"); ?></p>
-									</footer>
-							</article>
-
-						<?php endif; ?>
-
-					</div> <!-- end #main -->
-
-					<?php get_sidebar(); ?>
-
-				</div> <!-- end #inner-content -->
-
-			</div> <!-- end #content -->
+</div> <!-- end #content -->
 
 <?php get_footer(); ?>
